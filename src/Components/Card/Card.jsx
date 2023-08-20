912e688f3b115e412d13a6d413c8cbb7385c422c
RickyAnchores <ricky23anchores@gmail.com>
2023-08-20 21:40:49 +0100

    edit card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({image,title,text}) => {
   return (
-    <div className='Card text-white bg-slate-900 p-5 mt-5 rounded-md'>
+    <div className='Card text-white bg-slate-950 p-4 mt-5 rounded-md border-4 border-slate-500 border-opacity-20'>
         <img src={image} alt="pokeimg" />
         <h2>{title}</h2>
         <p>{text}</p>
